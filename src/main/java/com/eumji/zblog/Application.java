85f90fe38577aac5e48bd7a134400a9c9497f603
eumji025 <zombie_goo@yeah.net>
2017-04-09 22:29:25 +0800

    修改入口类

--- src/main/java/com/eumji/zblog/Application.java
+++ src/main/java/com/eumji/zblog/Application.java
@@ -1,5 +1,6 @@
 package com.eumji.zblog;
 
+import jdk.nashorn.internal.runtime.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,11 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * com.eumji.zblog.Application.java
  *
  * @desc 不积跬步无以至千里, 不积小流无以至千里
- * AUTHOR :EumJi
- * DATE: 2017/4/8
+ * @author:EumJi
+ * @year: 2017
+ * @month: 04
+ * @day: 02
+ * @time: 2017/4/2
  */
 @SpringBootApplication
+@Logger
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
