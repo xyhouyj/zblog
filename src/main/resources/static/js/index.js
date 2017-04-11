49b60d99e5f5ce2c43a02a1bd0aca46a0762c82c
eumji025 <zombie_goo@yeah.net>
2017-04-11 21:09:35 +0800

    添加所有资源

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,10 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>$Title$</title>
-</head>
-<body>
-$END$
-</body>
-</html>
+var pager = {start:0,limit:10};
+$(function () {
+    //初始化文章分页信息
+    $('.M-box').pagination({
+        callback:function(api){
+            $('.now').text(api.getCurrent());
+        }
+    },function(api){
+        $('.now').text(api.getCurrent());
+    });
+    //初始化文章
+    $.ajax({
+        type: 'GET',
+        url: '/article/aritcleDescList',
+        data:{pager:pager},
+        success: function (data){
+            queryResult = data.data;
+        }
+    });
+})
