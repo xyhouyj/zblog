0332329e77f531bb683dfe066ea2ed6fbb5db00f
eumji025 <zombie_goo@yeah.net>
2017-04-15 23:12:46 +0800

    开启日志存储操作

--- src/main/java/com/eumji/zblog/mapper/LogMapper.java
+++ src/main/java/com/eumji/zblog/mapper/LogMapper.java
@@ -1,6 +1,7 @@
 package com.eumji.zblog.mapper;
 
 import com.eumji.zblog.vo.LogInfo;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author Do
@@ -9,6 +10,7 @@ import com.eumji.zblog.vo.LogInfo;
  * @date 2017/4/10
  * @time 18:16
  */
+@Mapper
 public interface LogMapper {
     /**
      * 保存日志信息
