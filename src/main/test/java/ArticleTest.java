73bc72d847b85baa79b96c5a7f3a5d3704837d90
eumji025 <zombie_goo@yeah.net>
2017-04-16 22:35:50 +0800

    删除字体的引用

--- src/main/test/java/ArticleTest.java
+++ src/main/test/java/ArticleTest.java
@@ -1,5 +1,12 @@
 package PACKAGE_NAME;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 /**
  * @author Do
  * @package PACKAGE_NAME
@@ -8,4 +15,21 @@ package PACKAGE_NAME;
  * @time 13:00
  */
 public class ArticleTest {
+
+    @Test
+    public void getTime(){
+        for (int i = 0; i < 20; i++) {
+            System.out.println(System.currentTimeMillis());
+        }
+    }
+
+    @Test
+    public void timeTest(){
+        String yyyyMMdd = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        for (int i = 0; i < 10; i++) {
+            int random = new Random().nextInt(899)+100;
+            System.out.println(yyyyMMdd+random);
+        }
+
+    }
 }
