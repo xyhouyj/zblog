3d07b2c55230ce1cd48ef320d489af51a3c54dba
eumji025 <zombie_goo@yeah.net>
2017-04-17 22:34:26 +0800

    添加markdown文件解析

---
+++ src/main/resources/static/js/article-content.js
@@ -0,0 +1,24 @@
+/**
+ * Created by EumJi on 2017/4/16.
+ */
+$(function() {
+    var testEditormdView;
+
+    $.get("", function(markdown) {
+        testEditormdView = editormd.markdownToHTML("article-content", {
+            htmlDecode      : "style,script,iframe",  // you can filter tags decode
+            emoji           : true,
+            taskList        : true,
+            tex             : true,  // 默认不解析
+            flowChart       : true,  // 默认不解析
+            sequenceDiagram : true,  // 默认不解析
+        });
+
+        //console.log("返回一个 jQuery 实例 =>", testEditormdView);
+
+        // 获取Markdown源码
+        console.log(testEditormdView);
+
+        //alert(testEditormdView.getMarkdown());
+    });
+});
