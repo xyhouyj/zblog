e1a5844e0632125749416ca0ad0ba4a78ebcb5e0
eumji025 <zombie_goo@yeah.net>
2017-04-21 23:37:36 +0800

    markdown要求的相应体

---
+++ src/main/java/com/eumji/zblog/vo/PhotoResult.java
@@ -0,0 +1,39 @@
+package com.eumji.zblog.vo;
+
+/**
+ * FILE: com.eumji.zblog.vo.PhotoResult.java
+ * MOTTO:  不积跬步无以至千里,不积小流无以至千里
+ * AUTHOR: EumJi
+ * DATE: 2017/4/21
+ * TIME: 22:20
+ */
+public class PhotoResult {
+
+    private int success;
+    private String url;
+    private String message;
+
+    public int getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(int success) {
+        this.success = success;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
