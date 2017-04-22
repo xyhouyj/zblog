6dbf48d4fdb0d7cd0ec3cbcd19e4440bda743276
eumji025 <zombie_goo@yeah.net>
2017-04-22 23:25:25 +0800

    调整分享js的加载顺序

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,5 @@
 var pager = {start:0,limit:10};
+
 $(function () {
     //初始化文章
     $.ajax({
@@ -7,15 +8,6 @@ $(function () {
         data:{pager:pager},
         success: function (data){
             $("#main").html(data);
-            /*分享初始化*/
-            $("#socialShare").socialShare({
-                content: '这是我的个人博客内容',
-                url:'http://kiritor.github.io/2015/09/28/shiro-Authentication/',
-                title:'这是我的个人博客标题',
-                summary:'这是我的博客描述',
-                pic:'https://user-gold-cdn.xitu.io/2017/4/8/0bc6a6ba97cff243292f7c26560e71fb.gif'
-            });
-
             //初始化文章分页信息
             //初始化文章
             $.ajax({
@@ -34,6 +26,15 @@ $(function () {
                         }
                     },function(api){
                         $('.now').text(api.getCurrent());
+                    });
+
+                    /*分享初始化*/
+                    $(".socialShare").socialShare({
+                        content: '这是我的个人博客内容',
+                        url:'http://kiritor.github.io/2015/09/28/shiro-Authentication/',
+                        title:'这是我的个人博客标题',
+                        summary:'这是我的博客描述',
+                        pic:'https://user-gold-cdn.xitu.io/2017/4/8/0bc6a6ba97cff243292f7c26560e71fb.gif'
                     });
                 }
             });
