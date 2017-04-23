ae54beb0fff986bb0a255c3756924bffb90ea184
eumji025 <zombie_goo@yeah.net>
2017-04-23 14:16:56 +0800

    1.将分页渲染方法重构 2.分页传递pager后台接受不到值的bug解决

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,11 +1,11 @@
-var pager = {start:0,limit:10};
-
+var pager = {page:1,start:0,limit:10};
+var pager1 = {page:1,start:0,limit:10};
 $(function () {
     //初始化文章
     $.ajax({
         type: 'GET',
         url: '/article/load',
-        data:{pager:pager},
+        data:pager,
         success: function (data){
             $("#main").html(data);
             //初始化文章分页信息
@@ -13,20 +13,10 @@ $(function () {
             $.ajax({
                 type: 'GET',
                 url: '/pager/article/load',
-                data:{pager:pager},
+                data:pager,
                 success: function (data){
                     pager = data;
-                    $('.M-box').pagination({
-                        pagerCount:pager.totalPageNum, //	总页数
-                        current:pager.pager,//当前第几页
-                        showData:pager.limit,//每页显示的条数
-                        count:3,//	当前选中页前后页数
-                        callback:function(api){
-                            $('.now').text(api.getCurrent());
-                        }
-                    },function(api){
-                        $('.now').text(api.getCurrent());
-                    });
+                    initPage();
 
                     /*分享初始化*/
                     $(".socialShare").socialShare({
@@ -43,29 +33,65 @@ $(function () {
 
 })
 
+function initPage() {
+    $('.M-box').pagination({
+        pagerCount:pager.totalPageNum, //	总页数
+        current:pager.pager,//当前第几页
+        showData:pager.limit,//每页显示的条数
+        count:3,//	当前选中页前后页数
+        callback:function(api){
+            $('.now').text(api.getCurrent());
+        }
+    },function(api){
+        $('.now').text(api.getCurrent());
+    });
+}
+
 
 function loadCategory(categoryId) {
-    var loadPager = {start:0,limit:10};
+    var loadPager = {page:1,start:0,limit:10};
     $.ajax({
         type: 'GET',
         url: '/category/load/' + categoryId,
-        data: {pager: pager},
+        data: loadPager,
         success: function (data) {
             $("#main").html(data);
+
+            //初始化标签分页信息
+            $.ajax({
+                type: 'GET',
+                url: '/category/pager/'+categoryId,
+                data:loadPager,
+                success: function (data) {
+                    pager = data;
+                    initPage();
+                }
+            });
         }
     });
 
 }
 
 function loadTag(tagId) {
-    var loadPager = {start:0,limit:10};
+    var loadPager = {page:1,start:0,limit:10};
 
     $.ajax({
         type: 'GET',
         url: '/tag/load/'+tagId,
-        data: {pager: loadPager},
+        data: loadPager,
         success: function (data) {
             $("#main").html(data);
+
+            //初始化标签分页信息
+            $.ajax({
+                type: 'GET',
+                url: '/category/pager/'+tagId,
+                data:loadPager,
+                success: function (data) {
+                    pager = data;
+                    initPage();
+                }
+            });
         }
     });
 }
