833573bf26d60f8a65adcdf49ee38a993e222a73
eumji025 <zombie_goo@yeah.net>
2017-04-30 12:00:29 +0800

    全部提交,本地没有问题,服务器存在不能添加文章的情况

--- blog.sql
+++ blog.sql
@@ -4,7 +4,7 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50632
 Source Host           : localhost:3306
-Source Database       : mr
+Source Database       : zblog
 
 Target Server Type    : MYSQL
 Target Server Version : 50632
